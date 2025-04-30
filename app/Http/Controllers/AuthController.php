b289886a53bc8003d7bc02e92f4144c68f8eb055
thatgeekdev <unruffledlion@gmail.com>
2025-04-30 04:37:24 +0200

    feat(authentication): logging out

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -30,8 +30,11 @@ class AuthController extends Controller
         }
     }
 
-    public function destroy(string $id)
+    public function destroy()
     {
-        //
+        Auth::logout();
+        request()->session()->invalidate();
+        request()->session()->regenerateToken();
+        return redirect()->route('jobs.index');
     }
 }
--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -12,7 +12,44 @@
 </head>
 
 <body class="mx-auto mt-10 max-w-2xl bg-linear-to-r/srgb from-indigo-200 to-teal-400 text-slate-700">
-    {{ auth()->user()->name ?? 'Guest' }}
+<nav class=" mb-8  flex justify-between text-lg font-medium">
+    <ul class="flex space-x-2">
+        <li>
+            <a href="{{ route('jobs.index') }}" class="text-2xl font-bold text-slate-800">HOME</a>
+        </li>
+    </ul>
+    <ul class="flex space-x-2">
+        @auth
+            <li>
+                {{ auth()->user()->name ?? anynomus }}
+            </li>
+            <li>
+                <form action="{{ route('auth.destroy') }}" method="POST">
+                    @csrf
+                    @method('DELETE') {{-- method spoofing --}}
+                    <button class="text-sm flex py-1">
+                        | Logout 
+                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
+                            <path stroke-linecap="round" stroke-linejoin="round" d="M8.25 9V5.25A2.25 2.25 0 0 1 10.5 3h6a2.25 2.25 0 0 1 2.25 2.25v13.5A2.25 2.25 0 0 1 16.5 21h-6a2.25 2.25 0 0 1-2.25-2.25V15m-3 0-3-3m0 0 3-3m-3 3H15" />
+                          </svg>
+                          
+                    </button>    
+                </form>
+            </li>
+        @else
+            <li>
+                <a href="{{ route('auth.create') }}" class="text-sm flex py-1">
+                    Sign In
+                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
+                        <path stroke-linecap="round" stroke-linejoin="round" d="M15.75 9V5.25A2.25 2.25 0 0 0 13.5 3h-6a2.25 2.25 0 0 0-2.25 2.25v13.5A2.25 2.25 0 0 0 7.5 21h6a2.25 2.25 0 0 0 2.25-2.25V15M12 9l-3 3m0 0 3 3m-3-3h12.75" />
+                      </svg>                      
+                </a>
+            </li>
+        @endauth
+        
+    </ul>
+
+</nav>
     {{ $slot }}
 </body>
 
