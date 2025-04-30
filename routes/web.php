ac62c0326c0e65afc7838a014ce0ab5fa101dda7
thatgeekdev <unruffledlion@gmail.com>
2025-04-30 03:17:05 +0200

    feat(authentication): basing the controller and sign-in form

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\AuthController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\JobController;
 
@@ -7,4 +8,10 @@ Route::get('', fn()=>to_route('jobs.index'));
 
 Route::resource('jobs', JobController::class)->only(
     'index','show'
+);
+
+Route::get('login', fn()=>to_route('auth.create'))->name('login');
+
+Route::resource('auth', AuthController::class)->only(
+    'create','store'
 );
